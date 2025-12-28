a26c3a0d03e250c67c7cf0f95fdc3c8515a1b563
Estecka <estecka@galacsys.com>
2025-12-28 17:08:05 +0100

    Workign pack generation

--- src/main/java/fr/estecka/variantscit/commands/AssetGenCommands.java
+++ src/main/java/fr/estecka/variantscit/commands/AssetGenCommands.java
@@ -10,10 +10,19 @@ import fr.estecka.variantscit.assetgen.FilledTemplate;
 import fr.estecka.variantscit.assetgen.GeneratedResourcePack;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.command.CommandRegistryAccess;
 import net.minecraft.command.CommandSource;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.concurrent.CompletableFuture;
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.argument;
@@ -28,7 +37,7 @@ import static net.minecraft.command.argument.IdentifierArgumentType.identifier;
 public class AssetGenCommands
 {
 	static public final Identifier ID = Identifier.of(VariantsCitMod.MODID, "assetgen");
-
+	static public final String BAKED_PACK_DIR = "VCIT Baked AssetGen";
 	static public final String ASSET_ARG = "asset id";
 	
 	static public void	Register(){
@@ -36,6 +45,10 @@ public class AssetGenCommands
 	}
 
 	static public void	RegisterWith(CommandDispatcher<FabricClientCommandSource> dispatcher, CommandRegistryAccess registryAccess){
+		var pack = literal("createPack")
+			.executes(AssetGenCommands::CreatePack)
+			;
+
 		var peek = literal("peek")
 			.then(argument(ASSET_ARG, identifier())
 				.suggests(AssetGenCommands::AssetAutofill)
@@ -45,6 +58,7 @@ public class AssetGenCommands
 		var root = literal(VariantsCitMod.MODID)
 			.then(literal("assetgen")
 				.then(peek)
+				.then(pack)
 			);
 
 		dispatcher.register(root);
@@ -65,19 +79,100 @@ public class AssetGenCommands
 /* # Command Handlers                                                         */
 /******************************************************************************/
 
+	static private int Error(CommandContext<FabricClientCommandSource> context, String message){
+		context.getSource().sendError(Text.literal(message));
+		return -1;
+	}
+
 	static private int AssetDump(CommandContext<FabricClientCommandSource> context) throws CommandSyntaxException {
 		Identifier id = context.getArgument(ASSET_ARG, Identifier.class);
 		FilledTemplate resource = (FilledTemplate)GeneratedResourcePack.INSTANCE.GetAll().get(id);
 
-		if (resource == null){
-			context.getSource().sendError(Text.literal("No such asset: "+id.toString()));
-			return -1;
-		}
+		if (resource == null)
+			return Error(context, "No such asset: "+id.toString());
 
 		VariantsCitMod.LOGGER.info("{}:\n{}", id, resource.getString());
 
 		context.getSource().sendFeedback(Text.literal("Asset content was printed into the game's log."));
 		return 0;
+	}
+
+	static private int CreatePack(CommandContext<FabricClientCommandSource> context) throws CommandSyntaxException {
+		File base = FabricLoader.getInstance().getModContainer(VariantsCitMod.MODID).get().findPath("baked_pack_base").get().toFile();
+		File pack = FabricLoader.getInstance().getGameDir().resolve("resourcepacks/" + BAKED_PACK_DIR).toFile();
+
+		if (!base.exists() || !base.isDirectory())
+			return Error(context, "Missing base for baked assetgen pack.");
+
+		// Delete
+		if (pack.exists()){
+			if (!pack.isDirectory())
+				return Error(context, "File \""+BAKED_PACK_DIR+"\" already exists but is not a directory.");
+			else {
+				boolean deleted = false;
+				context.getSource().sendFeedback(Text.literal("Deleting existing baked pack..."));
+				try {
+					deleted = pack.delete();
+				}
+				catch (SecurityException e){
+					VariantsCitMod.LOGGER.error("Unabled to delete baked assetgen pack:\n{}", e);
+					return Error(context, "Unable to delete baked assetgen pack.");
+				}
+
+				if (!deleted)
+					return Error(context, "Unable to delete baked assetgen pack.");
+			}
+		}
+
+		// Init
+		try {
+			CopyRecursive(base.toPath(), pack.toPath());
+		}
+		catch (IOException e){
+			VariantsCitMod.LOGGER.error("Unable to initialize baked pack:\n{}", e);
+			return Error(context, "Unable to initialize baked pack.");
+		}
+
+		// Assets
+		context.getSource().sendFeedback(Text.literal("Writing assets..."));
+		boolean error = false;
+		for (var entry : GeneratedResourcePack.INSTANCE.GetAll().entrySet()){
+			Identifier id = entry.getKey();
+			String assetPath = "assets/"+id.getNamespace()+"/"+id.getPath();
+			Path assetDst = pack.toPath().resolve(assetPath);
+
+			assetDst.getParent().toFile().mkdirs();
+
+			try {
+				Files.write(assetDst, entry.getValue().get().readAllBytes());
+			}
+			catch (IOException e){
+				VariantsCitMod.LOGGER.error("Error writing file: {}\n{}", assetDst, e);
+			}
+		}
+
+		if (error)
+			return Error(context, "Error while writing some assets. See log for details.");
+
+		context.getSource().sendFeedback(Text.literal("Done !"));
+		return 1;
+	}
+
+	static private void CopyRecursive(Path src, Path dst)
+	throws IOException
+	{
+		Files.walkFileTree(src, new SimpleFileVisitor<Path>(){
+			@Override
+			public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+				Files.createDirectories(dst.resolve(src.relativize(dir)));
+				return FileVisitResult.CONTINUE;
+			};
+			@Override
+			public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+				Files.copy(file, dst.resolve(src.relativize(file)), StandardCopyOption.REPLACE_EXISTING);
+				return FileVisitResult.CONTINUE;
+			};
+		} );
 	}
 
 }
